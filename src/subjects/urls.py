118decabdfb48e2c16be8ba2fef3882743c56420
NhanNT <nhannt.bk@gmail.com>
2017-08-03 08:41:07 +0700

    Update

--- src/subjects/urls.py
+++ src/subjects/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import SubjectListView
 
+app_name = 'student'
+
 urlpatterns = [
     url(r'^transcript/$', SubjectListView.as_view(), name='transcript'),
 ]
