15a73d1ceff8840dcadd05e18db1a3494877ccbb
NhanNT <nhannt.bk@gmail.com>
2017-08-15 10:37:22 +0700

    Update

--- src/student_info_site/settings_secret.py
+++ src/student_info_site/settings_secret.py
@@ -5,5 +5,6 @@ SERVER_EMAIL = 'cyborg31596@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cyborg31596@gmail.com'
-EMAIL_HOST_PASSWORD = 'nishinos169'
+EMAIL_HOST_PASSWORD = 'nishino169'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
